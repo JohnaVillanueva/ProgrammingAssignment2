5d07e266a3eb7546352fa372131316f54e73f4ed
JohnaVillanueva <mjrcvillanueva@mymail.mapua.edu.ph>
2021-07-10 15:03:57 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,5 @@
 
-##
-## makeCacheMatrix is composed of set, get, setinverse and getinverse
-##This function helps to get and set the inverse function of a matrix
-##Also, helps to get and set the value of the matrix
-##cacheSolve is use to get the cached data of a dataset
-##Overall, the presented function is purposely use to get data on faster operation
+
 makeCacheMatrix <- function(x = matrix()) {
  g<-NULL   #making the g as NULL
  f<-function(y){
