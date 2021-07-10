392fef7c31675646aa50b49c8df40de2d8020028
JohnaVillanueva <mjrcvillanueva@mymail.mapua.edu.ph>
2021-07-10 12:06:48 +0800

    Update cachematrixx.R

--- cachematrixx.R
+++ cachematrixx.R
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
@@ -24,52 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 ##To get the cache data, cacheSolve is used
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) { ##This function gets the cache data from the presented data
   g<-x$getmean()
-  if(!is.null(g)){
+  if(!is.null(g)){               ##This function is used to check if the inverse is null
     message("getting cached data")
-    return(g)
+    return(g)                    ##This function returns the inverse value of "g"
   }
   data<-x$get()
   g<-solve(data, ...)
   x&setinv(g)
   g      ## Return a matrix that is the inverse of 'x'
 }
-=======
-## Put comments here that give an overall description of what your
-## functions do
-
-## makeCacheMatrix is composed of set, get, setinverse and getinverse
-
-makeCacheMatrix <- function(x = matrix()) {
- g<-NULL   #making the g as NULL
- f<-function(y){
-    x<<-y
-    g<<-NULL
- }
- get<-function()x
- setinv<-function(inverse)f<<-inverse
- getinv<-function()g{
-   
- }
- list(set = set, get = get,
-  setinverse = setinverse,
-  getinverse = getinvserse)
-}
-
-
-## Write a short comment describing this function
-##To get the cache data, cacheSolve is used
-
-cacheSolve <- function(x, ...) {
-  g<-x$getmean()
-  if(!is.null(g)){
-    message("getting cached data")
-    return(g)
-  }
-  data<-x$get()
-  g<-solve(data, ...)
-  x&setinv(g)
-  g      ## Return a matrix that is the inverse of 'x'
-}
->>>>>>> 306acd580564b1181839f3b7d66e656fcf046d30
