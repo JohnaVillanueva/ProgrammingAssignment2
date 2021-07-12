0244e0a473fac7097d93038a7d7601b9c96d8eb6
JohnaVillanueva <mjrcvillanueva@mymail.mapua.edu.ph>
2021-07-12 14:38:42 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,80 @@
+##The presented command aims to acquire the set inverse function of the matrix 
+##and value of the matrix.
+##Also, the get of the value of the matrix and the inverse of the matrix.
+##The main purpose of the function is to get the necessary data in a faster operation.
+
+##Below, the lan represents the inverse
+##the san represents the overall set of the matrix
+##lan is perceived as null
+
+
 makeCacheMatrix <- function(x = matrix()) {
- lan<-NULL   #making the g as NULL
- san<-function(y){
-         x<<-y
-         lan<<-NULL
-         }
- get<-function()x
- setinv<-function(inverse)san<<-inverse
- getinv<-function(){lan}
- list(set = set, get = get, setinverse = setinverse, getinverse = getinvserse)
+  
+  
+  
+  lan<-NULL
+  
+  
+  san<-function(y){
+    
+    x<<-y
+    
+    lan<<-NULL
+    
+    
+  }
+  
+  
+  get<-function()x
+  
+  
+  setinv<-function(inverse)san<<-inverse
+  
+  
+  getinv<-function(){lan}
+  
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinvserse)
+  
+  
 }
 
-##It uses the command 'solve' to compute the inverse of the 'x'
 
-cacheSolve <- function(x, ...) {  #This function gets the cache data from the presented data
+##The presented command is used to calculate the inverse of 'x' 
+##which is represnted by the word "lan
+##and the cache of the presented data
+
+##The command also used to determine the nullity of the inverse function
+## The command is used to examine for the returning of the inverse  value of "lan"
+
+
+
+cacheSolve <- function(x, ...) {  
+  
+  
   lan<-x$getmean()
-  if(!is.null(lan)){                   #This function is used to check if the inverse is null
-          message("getting cached data")
-         return(g)                         #This function returns the inverse value of "g"
+  
+  
+  if(!is.null(lan)){ 
+    
+    message("getting cached data")
+    
+    return(lan)      
+    
   }
+  
+  
   data <-x$get()
+  
+  
   lan <-solve(data, ...)
+  
+  
   x&setinv(lan)
-  lan          ## Return a matrix that is the inverse of 'x'
+  
+  
+  lan      
+  
+}
 }
 
